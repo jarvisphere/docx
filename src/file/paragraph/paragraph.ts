cab47930eeff5a69cdb5f0193a7f3556549872c4
Hemant <hemantsaini95689@gmail.com>
2026-02-27 01:50:08 +0530

    added citation support

--- src/file/paragraph/paragraph.ts
+++ src/file/paragraph/paragraph.ts
@@ -11,6 +11,7 @@ import type { IContext, IXmlableObject } from "@file/xml-components";
 import { uniqueId } from "@util/convenience-functions";
 
 import type { CheckBox } from "../checkbox";
+import { CitationField } from "../citation/index";
 import { TargetModeType } from "../relationships/relationship/relationship";
 import type { DeletedTextRun, InsertedTextRun } from "../track-revision";
 import type { ColumnBreak, PageBreak } from "./formatting/break";
@@ -54,7 +55,8 @@ export type ParagraphChild =
     | CommentRangeStart
     | CommentRangeEnd
     | CommentReference
-    | CheckBox;
+    | CheckBox
+    | CitationField;
 
 /**
  * Options for creating a Paragraph element.
